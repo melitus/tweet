f7dd829c6d1dbe830cf8500ad95a03a6630ac78b
melitus <asmelitus@gmail.com>
2022-06-16 14:14:21 +0100

    feat: add implemntation for Pet train on Newsarticle

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,22 @@ pub struct NewArticles {
     pub content: String,
     pub headline: String,
  }
+
+ impl Summary for NewArticles {
+    fn summary(&self) -> String {
+        format!("{} by {}", self.headline, self.author) 
+    }
+}
+
 pub struct Tweets { 
     pub username: String, 
     pub content: String, 
     pub reply: bool,
     pub retweet: bool 
+}
+
+pub trait Summary {
+    fn summary(&self) -> String;
 }
 
 fn main() {
